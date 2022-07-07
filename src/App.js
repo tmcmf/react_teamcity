f2b27b26565e7393c7af41da2c7de3abc28fdc38
sundar21 <sundar@nife.io>
2022-07-07 15:04:18 +0530

    first_push

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import "./App.css";
+import { useState } from "react";
+function App() {
+  const [details, setDetails] = useState({
+    name: "",
+    age: null,
+  });
+  const [error, setError] = useState("");
+  const handleSubmit = () => {
+    console.log(details);
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    if (name === "age") {
+      setDetails({ ...details, age: value });
+      if (value >= 18) {
+        setError("");
+      } else {
+        setError("Please enter valid age");
+      }
+    } else {
+      setDetails({ ...details, name: value });
+    }
+  };
+  return (
+    <div>
+      <div className="container">
+        <div className="mb-3 mt-3">
+          <label for="name">Name:</label>
+          <input
+            type="name"
+            className="form-control"
+            id="name"
+            value={details.name}
+            placeholder="Enter name"
+            name="name"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="mb-3">
+          <label for="age">Age:</label>
+          <input
+            type="number"
+            className="form-control"
+            id="age"
+            placeholder="Enter age"
+            name="age"
+            value={details.age}
+            onChange={handleChange}
+          />
+        </div>
+        <p className="text-danger">{error}</p>
+        <div className="form-check mb-3">
+          <label className="form-check-label">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              name="remember"
+            />{" "}
+            Remember me
+          </label>
+        </div>
+        <button
+          type="submit"
+          className="btn btn-primary"
+          onClick={handleSubmit}
+        >
+          Submit
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
